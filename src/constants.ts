037740df7448fea569946a4008750657c342d9be
MilaKoseckaya <kosetsaya@gmail.com>
2025-11-08 17:31:49 +0200

    add user controller

--- src/constants.ts
+++ src/constants.ts
@@ -20,6 +20,7 @@ export const METHODS = {
 export const STATUS_CODES = {
   OK: 200,
   CREATED: 201,
+  NO_CONTENT: 204,
   BAD_REQUEST: 400,
   NOT_FOUND: 404
 };
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import * as http from 'http';
-import { isValidUUID, validateUserData } from './utils/validation';
 import * as dotenv from 'dotenv';
-import { usersDB } from './Users/users';
 import { DEFAULT_PORT, ERROR_MSG, METHODS, STATUS_CODES } from './constants';
 
 import { sendResponse } from './utils/response';
+import { UsersController } from './controllers/users.controller';
 
 dotenv.config();
 
@@ -14,58 +13,26 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'application/json');
 
   if (req.method === METHODS.GET && req.url === '/api/users') {
-    const users = usersDB.getAllUsers();
-    return sendResponse(res, STATUS_CODES.OK, users);
+    return UsersController.getAllUsers(res);
   }
 
   if (req.method === METHODS.GET && req.url?.startsWith('/api/users/')) {
     const userId = req.url.split('/api/users/')[1];
-
-    if (!isValidUUID(userId)) {
-      return sendResponse(res, STATUS_CODES.BAD_REQUEST, {
-        message: ERROR_MSG.INVALID_UUID
-      });
-    }
-
-    const user = usersDB.getUserById(userId);
-    if (!user) {
-      return sendResponse(res, STATUS_CODES.NOT_FOUND, {
-        message: ERROR_MSG.USER_NOT_FOUND
-      });
-    }
-
-    return sendResponse(res, STATUS_CODES.OK, user);
+    return UsersController.getUserById(res, userId);
   }
-  console.log('request ', req.method, req.url);
+
   if (req.method === METHODS.POST && req.url === '/api/users') {
-    console.log('POST request received', req.url);
-    let body = '';
+    return UsersController.createUser(req, res);
+  }
 
-    req.on('data', (chunk) => {
-      body += chunk.toString();
-    });
+  if (req.method === METHODS.PUT && req.url?.startsWith('/api/users/')) {
+    const userId = req.url.split('/api/users/')[1];
+    return UsersController.updateUser(req, res, userId);
+  }
 
-    req.on('end', () => {
-      try {
-        const parsedData = JSON.parse(body);
-        const validationResult = validateUserData(parsedData);
-
-        if (!validationResult.isValid) {
-          return sendResponse(res, STATUS_CODES.BAD_REQUEST, {
-            message: validationResult.message
-          });
-        }
-
-        const newUser = usersDB.addUser(validationResult.data);
-        return sendResponse(res, STATUS_CODES.CREATED, newUser);
-      } catch (error) {
-        return sendResponse(res, STATUS_CODES.BAD_REQUEST, {
-          message: ERROR_MSG.INVALID_JSON
-        });
-      }
-    });
-
-    return;
+  if (req.method === METHODS.DELETE && req.url?.startsWith('/api/users/')) {
+    const userId = req.url.split('/api/users/')[1];
+    return UsersController.deleteUser(res, userId);
   }
 
   sendResponse(res, STATUS_CODES.NOT_FOUND, {
